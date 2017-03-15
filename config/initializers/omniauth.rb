b7203a80cdb4963732502822b74954e87e4157a1
Chiranjiv-Roy <chiranjiv.roy@lexmark.com>
2017-03-16 00:12:46 +0530

    navbar changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 require "builder"
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: 'email,user_friends'
 end
